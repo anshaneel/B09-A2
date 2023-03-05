8dbf2abaa05e6aea359ca882e96cffb74d30442a
Ansh Aneel <anshaneel@hotmail.com>
2023-03-05 17:28:18 -0500

    Implementation is done need to thouroughly test now

--- SystemTables.c
+++ SystemTables.c
@@ -49,6 +49,10 @@ void getFileDescriptors(process_info processes[1024], char path[1024], int* coun
                     exit(EXIT_FAILURE);
                 }
 
+                if (file_stat.st_ino <= 0){
+                    continue;
+                }
+
                 processes[*count].PID = pid;
                 processes[*count].FD = fd;
                 strncpy(processes[*count].file_name, link, 1024);
@@ -62,7 +66,7 @@ void getFileDescriptors(process_info processes[1024], char path[1024], int* coun
     closedir(dp2);
 }
 
-int getProcesses(process_info processes[1024], bool specific_process_chosen, int process_chosen){
+int getProcesses(process_info processes[1024], bool specific_process_chosen, int process_chosen, int threshold){
 
     //Populates the process array with all the current running processes
     int count = 0;
@@ -73,6 +77,7 @@ int getProcesses(process_info processes[1024], bool specific_process_chosen, int
     proc = opendir("/proc");
 
     if (specific_process_chosen){
+        snprintf(path, sizeof(path), "/proc/%d/fd", process_chosen);
         getFileDescriptors(processes, path, &count, process_chosen);
         return count;
     }
@@ -82,7 +87,7 @@ int getProcesses(process_info processes[1024], bool specific_process_chosen, int
             // Gets pid of process
             pid_t pid = atoi(ep -> d_name);
             
-            if (pid <= 0){
+            if (pid <= threshold){
                 continue;
             }
             // Create path to access file descriptors of process then calls function getFileDescrioptors to add the information to processes
@@ -98,14 +103,14 @@ int getProcesses(process_info processes[1024], bool specific_process_chosen, int
 
 void outputTXT(process_info processes[1024], int count){
 
-    File *fp = fopen("compositeTable.txt", "r");
+    FILE *fp = fopen("compositeTable.txt", "r");
     fprintf(fp, "%s\n", "         PID     FD      Filename                Inode");
     fprintf(fp, "%s\n", "        ===============================================");
 
     for (int i = 0; i < count; i++){
         fprintf(fp, "%d        %d  %d        %s        %d\n", i, processes[i].PID, processes[i].FD, processes[i].file_name, processes[i].Inode);
     }
-    fprintf("%s\n", "        ===============================================");
+    fprintf(fp, "%s\n", "        ===============================================");
     fclose(fp);
 
 }
@@ -137,7 +142,7 @@ void outputBinary(process_info processes[1024], int count){
     fwrite(footer, sizeof(footer), 1, fp);
 
     fclose(fp);
-    
+
 }
 
 void displayComposite(process_info processes[1024], int count){
@@ -148,7 +153,7 @@ void displayComposite(process_info processes[1024], int count){
     for (int i = 0; i < count; i++){
         printf("%d        %d  %d        %s        %d\n", i, processes[i].PID, processes[i].FD, processes[i].file_name, processes[i].Inode);
     }
-    printf("%s\n", "        ===============================================");
+    printf("%s\n\n", "        ===============================================");
 }
 
 void displayVnodes(process_info processes[1024], int count){
@@ -159,7 +164,7 @@ void displayVnodes(process_info processes[1024], int count){
     for (int i = 0; i < count; i++){
         printf("%d        %d              %d\n", i, processes[i].FD, processes[i].Inode);
     }
-    printf("%s\n", "        ===============================================");
+    printf("%s\n\n", "        ===============================================");
 }
 
 void displaySystemWide(process_info processes[1024], int count){
@@ -170,7 +175,7 @@ void displaySystemWide(process_info processes[1024], int count){
     for (int i = 0; i < count; i++){
         printf("%d        %d  %d        %s\n", i, processes[i].PID, processes[i].FD, processes[i].file_name);
     }
-    printf("%s\n", "        ===============================================");
+    printf("%s\n\n", "        ===============================================");
 }
 
 void displayPerProcess(process_info processes[1024], int count){
@@ -181,7 +186,30 @@ void displayPerProcess(process_info processes[1024], int count){
     for (int i = 0; i < count; i++){
         printf("%d        %d  %d\n", i, processes[i].PID, processes[i].FD);
     }
-    printf("%s\n", "        ============");
+    printf("%s\n\n", "        ============");
+}
+
+void display(process_info processes[1024], int count, bool per_process, bool systemWide, bool Vnodes, bool composite, bool specific_process_chosen, bool txt, bool bin){
+
+    if (composite){
+        displayComposite(processes, count);
+    }
+    if (systemWide){
+        displaySystemWide(processes, count);
+    }
+    if (Vnodes){
+        displayVnodes(processes, count);
+    }
+    if (per_process){
+        displayPerProcess(processes, count);
+    }
+    if (txt){
+        outputTXT(processes, count);
+    }
+    if (bin){
+        outputBinary(processes, count);
+    }
+
 }
 
 int main(int argc, char* argv[]){
@@ -189,26 +217,32 @@ int main(int argc, char* argv[]){
     process_info processes[1024];
 
     int threshold; int process_chosen;
-    bool selected = false, per_process = false, systemWide = false, Vnodes = false, composite = false, specific_process_chosen = false;
+    bool selected, per_process, systemWide, Vnodes, composite, specific_process_chosen, txt, bin;
 
     for (int i = 1; i < argc; i++){
-        if (strcmp(argv[i], "--per-process") == 0 && !selected){
+        if (strcmp(argv[i], "--per-process") == 0){
             per_process = true;
+            selected = true;
         }
-        else if (strcmp(argv[i], "--systemWide") == 0 && !selected){
+        else if (strcmp(argv[i], "--systemWide") == 0){
             systemWide = true;
+            selected = true;
         }
-        else if (strcmp(argv[i], "--Vnodes") == 0 && !selected){
+        else if (strcmp(argv[i], "--Vnodes") == 0){
             Vnodes = true;
+            selected = true;
         }
-        else if (strcmp(argv[i], "--composite") == 0 && !selected){
+        else if (strcmp(argv[i], "--composite") == 0){
             composite = true;
+            selected = true;
         }
         else if (strcmp(argv[i], "--output_TXT") == 0){
-
+            txt = true;
+            selected = true;
         }
         else if (strcmp(argv[i], "--output_binary") == 0){
-
+            bin = true;
+            selected = true;
         }
         else if (strncmp(argv[i], "--threshold=", 12) == 0){
             threshold = atoi(argv[i] + 12);
@@ -219,6 +253,11 @@ int main(int argc, char* argv[]){
         }
     }
 
-    int count = getProcesses(processes, specific_process_chosen, process_chosen);
-    displaySystemWide(processes, count);
+    if (specific_process_chosen && !selected){
+        composite = true;
+    }
+
+    int count = getProcesses(processes, specific_process_chosen, process_chosen, threshold);
+    display(processes , count, per_process, systemWide, Vnodes, composite, specific_process_chosen, txt, bin);
+
 }
